39cb9bb2ef3fe88dd8002e7a6788b43de4644000
Mark Riley Bomberry <higgsofwolfram74@gmail.com>
2022-02-05 20:01:04 -0600

    Starting to implement driver code for binary

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,57 @@
-#![allow(dead_code, unused_imports)]
+#![allow(dead_code, unused_imports, unused_variables)]
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
 mod wordanalyzer;
+//TODO: When given a yellow letter, we know where that letter isn't. We could filter that too.
 
 fn main() {
     let path: &str = "./fiveletterwords.txt";
     let file = File::open(path)
-                    .unwrap_or_else(|_| panic!("File not found at {}, please recheck your path.", path));
+        .unwrap_or_else(|_| panic!("File not found at {}, please recheck your path.", path));
 
     let reader = BufReader::new(file);
 
-    let read: Vec<String> = reader.lines().map(|x| x.unwrap().trim_end().to_string()).collect();
+    let read: Vec<String> = reader
+        .lines()
+        .map(|x| x.unwrap().trim_end().to_string())
+        .collect();
 
-    println!("{}", read[0]);
+    let lexicon: wordanalyzer::WordAnalyzer = wordanalyzer::WordAnalyzer::new(read);
+
+    println!("Welcome to my Wordle Solver!\nI appreciate you using my program :)\nPlease input the word \"weary\" and record the result.");
+
+    println!("Of the word weary, please put in any green letters in their place.\n Put a _ for any placeholders");
+
+    let mut letters = String::new();
+
+    io::stdin()
+        .read_line(&mut letters)
+        .expect("Failed to read line.");
+
+    while letters.len() != 5 {
+        println!()
+    }
+}
+
+fn green_input(inp: &String) -> Vec<(usize, char)> {
+    let mut result: Vec<(usize, char)> = Vec::new();
+    for elem in inp.char_indices() {
+        if elem.1.is_alphabetic() && elem.1.is_ascii() {
+            result.push(elem);
+        }
+    }
+    result
+}
+
+fn other_input(inp: &String) -> Vec<char> {
+    let mut result: Vec<char> = Vec::new();
+    for letter in inp.chars() {
+        if letter.is_alphabetic() && letter.is_ascii() {
+            result.push(letter);
+        }
+    }
+    result
 }
--- src/wordanalyzer.rs
+++ src/wordanalyzer.rs
@@ -1,6 +1,5 @@
 pub struct WordAnalyzer {
     words: Vec<String>,
-    
 }
 
 pub struct LettersManager {
@@ -10,12 +9,23 @@ pub struct LettersManager {
 }
 
 impl LettersManager {
-    pub fn new(greens: Vec<(usize, char)>, yellows: Vec<char>, blacks: Vec<char>) -> Self {
+    pub fn new() -> Self {
         Self {
-            g_letters: greens,
-            y_letters: yellows,
-            b_letters: blacks
+            g_letters: Vec::new(),
+            y_letters: Vec::new(),
+            b_letters: Vec::new(),
         }
+    }
+
+    pub fn set(
+        &mut self,
+        g_letters: Vec<(usize, char)>,
+        y_letters: Vec<char>,
+        b_letters: Vec<char>,
+    ) -> () {
+        self.g_letters = g_letters;
+        self.y_letters = y_letters;
+        self.b_letters = b_letters;
     }
 
     pub fn get_green(&self) -> &Vec<(usize, char)> {
@@ -32,52 +42,36 @@ impl LettersManager {
 }
 
 impl WordAnalyzer {
-
     pub fn new(wordlist: Vec<String>) -> Self {
-        Self {
-            words: wordlist,
-        }
+        Self { words: wordlist }
     }
 
-    pub fn filter_words(self, letters: LettersManager) -> Self {
-        let mut initial_words: Vec<String> = self.words;
-        let lets: &LettersManager = &letters;
-        initial_words = WordAnalyzer::green_letters(initial_words, lets);
-        initial_words = WordAnalyzer::yellow_letters(initial_words, lets);
-
-        return Self {
-            words: initial_words
-        }
+    pub fn filter_words(&mut self, letters: LettersManager) -> () {
+        self.words
+            .retain(|x| WordAnalyzer::word_checker(x, &letters));
     }
 
-    fn green_letters(words: Vec<String>, letters: &LettersManager) -> Vec<String> {
-        words.into_iter().filter(|x| WordAnalyzer::word_checker_green(x, letters)).collect()      
-    }
+    fn word_checker(word: &String, letters: &LettersManager) -> bool {
+        // Each guess produces five letters we know, so iteration here is negligible imo
 
-    fn yellow_letters(words: Vec<String>, letters: &LettersManager) -> Vec<String> {
-        words.into_iter().filter(|x| WordAnalyzer::word_checker_by(x, letters)).collect()
-    }
-
-    fn word_checker_green(word: &String, letters: &LettersManager) -> bool{
         for letter in letters.get_green() {
             if char::from_u32(word.as_bytes()[letter.0] as u32).unwrap() != letter.1 {
                 return false;
-            } 
+            }
         }
-        return true;
-    }
 
-    fn word_checker_by(word: &String, letters: &LettersManager) -> bool {
         for letter in letters.get_black() {
             if word.contains(*letter) {
                 return false;
             }
         }
+
         for letter in letters.get_yellow() {
             if !word.contains(*letter) {
-                return false
+                return false;
             }
         }
+
         return true;
     }
 }
